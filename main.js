5963f255772ded7cbdfaab952ba60146e698f932
travismcminn <travis@stitchitintl.com>
2025-09-30 13:37:36 -0500

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-  const mobileBtn = document.querySelector('#mobileMenuBtn');
-  const mobileMenu = document.querySelector('#mobileMenu');
-  if (mobileBtn && mobileMenu) {
-    mobileBtn.addEventListener('click', () => mobileMenu.classList.toggle('hidden'));
-  }
-});
